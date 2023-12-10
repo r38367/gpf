e30ae1b329a33ec4960455f91c1dd8f5fc51dfd5
r38367 <r38367@gmail.com>
2023-12-10 01:14:28 +0100

    Update ds.py

--- ds.py
+++ ds.py
@@ -2,13 +2,17 @@
 
 # History
 #
-# v1
-# 03/12/23 add #1 open/edit jpg button  
-# 04/12/23 add #5 clear button
-# 05/12/23 add #4 open/edit word button
-# 05/12/23 add #11 clear with double click on label
+# v1.0
+#   03/12/23 add #1 open/edit jpg button  
+#   04/12/23 add #5 clear button
+#   05/12/23 add #4 open/edit word button
+#   05/12/23 add #11 clear with double click on label
+#
+# v2.0
+#   09/12/23 add #6 add file list in inout field
+#
 
-
+nVer = '2.0'
 
 # Import Module
 from tkinter import *
@@ -52,9 +56,7 @@ def makescreenshotname( t, text="screen" ):
         os.mkdir(subfolder)
 
     return   subfolder + '\\' + t.replace(':','').replace(' ','_') + "_" + text +".png"
-
-
-
+ 
 
 # ==============================================================================================================
 # working with word document 
@@ -118,6 +120,7 @@ def makewordname( input ):
         # 1234 filename2 6000313 =filename2
         # my file 6000313 2546 =my file
         # 2456 6000313 G02056 =G02056
+        # 3333 anton 6000777 delete =
         #
         non_digit = re.findall(r'\b([^\d\s].*?)(?:\s+\d+)*$', input )
         if not non_digit:
@@ -140,27 +143,33 @@ def makewordname( input ):
 #  Call backs 
 # ==============================================================================================================
 
-nVer = '4.0'
 test = 0
 policy = 0
 wordname = ""
 jpg = ""
 
 # ==============================================================================================================
-#                   Input entered 
+#                   Input 
+# pressed  
 # ==============================================================================================================
    
 def _input_pressed(event):
     global wordname 
     
+    # hide listbox
+    listbox.grid_remove()
+    # get text from input 
     res = txt.get().strip()
+    # make file name from input 
     wordname = makewordname( res )
-    
+    # set label to file name 
     lbl.configure(text = wordname)
 
 
 # ==============================================================================================================
-#                   Take button pressed 
+#                   Take button 
+# 
+# pressed/released/changed
 # ==============================================================================================================
   
 _pressed = 0  # 0-not pressed, 1-short, 2-long
@@ -178,7 +187,7 @@ def _take_button_pressed( event ):
 
     if _pressed == 0 :
         _pressed = 1
-        _long = root.after(800,_change_label,event)
+        _long = root.after(700,_change_label,event)
         
 
 def _take_button_released( event ):
@@ -219,7 +228,9 @@ def _take_button_released( event ):
 
 
 # ==============================================================================================================
-#                   Open button pressed 
+#                   Open button
+# 
+#            pressed/released/changed
 # ==============================================================================================================
 
 _pressed_open = 0  # 0-not pressed, 1-short, 2-long
@@ -239,7 +250,7 @@ def _open_button_pressed( event ):
     
     if _pressed_open == 0 :
         _pressed_open = 1
-        _long = root.after(800,_open_change_label,event)
+        _long = root.after(700,_open_change_label,event)
         
 
 def _open_button_released( event ):
@@ -256,8 +267,6 @@ def _open_button_released( event ):
     global wordname 
     global jpg
 
-   
-    
     # open picture/word
     if _pressed_open == 2:
         
@@ -281,13 +290,112 @@ def _open_button_released( event ):
    
 
 # ==============================================================================================================
-#                   Clear button pressed 
+#                  Clear button 
+# pressed 
 # ==============================================================================================================
 
-def _clear_button_pressed(event=0):
+def _clear_button_pressed(event=None):
     
     txt.delete(0, END)
     lbl.configure(text = "")
+    listbox.grid_remove()
+
+# ==============================================================================================================
+#                   Listbox - update
+#
+# update after input text in input field  
+# ==============================================================================================================
+
+def update_listbox (event=None):
+    
+    if event.keysym == "Return":
+        # Move the focus to the listbox
+        listbox.grid_remove()
+        return
+    
+    # Clear the listbox
+    listbox.delete (0, END)
+    # Get the input text
+    input_text = txt.get ().strip()
+    #check if string is empty
+    if not input_text:
+        return 
+    # Initialize a counter for the number of matching files
+    count = 0
+    # Loop through the files in the current folder
+    for file in os.listdir ("."):
+        # Check if the file name contains the input text
+        if all(file.upper().find(str) >= 0  for str in input_text.upper().split()):
+            # Insert the file name into the listbox
+            listbox.insert (END, file)
+            # Increment the counter
+            count += 1
+    # Resize the listbox height according to the counter
+    listbox.config (height=min(count,5)) 
+    if count < 1:
+        listbox.grid_remove()
+    else:
+        listbox.grid(column=0, row=1)
+    
+        
+# ==============================================================================================================
+#                   Listbox - return from listbox
+# 
+# Create a function to update the entry when press Return in listbox
+# ==============================================================================================================
+def update_entry (event):
+    
+    # Check if the event is triggered by pressing ENTER
+    if event.keysym == "Return":
+        # Get the selected file name from the listbox
+        file_name = listbox.get (listbox.curselection ())
+        # hide listbox
+        listbox.grid_remove()
+        # Set the input text to the file name
+        txt.delete(0, END)
+        txt.insert (0, file_name.replace('_',' ').rsplit('.',1)[0])
+        # Update the listbox
+        #update_listbox ()
+        
+        # Hide the toplevel window 
+        # Changing position of cursor to end 
+        txt.icursor(END)
+        # set focus to entry
+        txt.focus()
+        #top.withdraw ()
+        _input_pressed(None)
+        
+
+# ==============================================================================================================
+#                   Listbox - show file name 
+# 
+# Show the active entry from listbox in the input  
+# ==============================================================================================================
+def show_file_name (event):
+    # Get the selected file name from the listbox
+    file_name = listbox.get (listbox.curselection ())
+    # Set the input text to the file name
+    txt.delete(0, END)
+    txt.insert (0, file_name.replace('_',' ').rsplit('.',1)[0])
+
+# ==============================================================================================================
+#                   Listbox - move focus 
+# 
+# Create a function to move the focus to the listbox
+# ==============================================================================================================
+def move_focus (event):
+    # Check if the event is triggered by pressing DOWN
+    if event.keysym == "Down":
+        # Move the focus to the listbox
+        listbox.focus ()
+        # Select the first item in the listbox
+        listbox.selection_set (0)
+        
+    if event.num == 1:
+        index = listbox.nearest(event.y)
+        value = listbox.get(index)
+        listbox.selection_set (index)
+        
 
 # ==============================================================================================================
 # Create GUI
@@ -299,7 +407,7 @@ root = Tk()
 root.title("Test DinSide - v." + nVer )
 
 # Set geometry(widthxheight)
-root.geometry('400x70')
+root.geometry('') 
 
 # Set margins inside window
 root['padx']=10
@@ -327,16 +435,29 @@ root.attributes('-topmost',True)
 #
 #  create input widget
 #
+
+# Create an entry widget
 txt = Entry(root, width=40)
+# place top left
 txt.grid(column =0, row =0, padx=5, pady=5)
+# Bind to Return key
 txt.bind('<Return>', _input_pressed)
+# Bind the entry widget to the update function
+txt.bind ("<KeyRelease>", update_listbox )
+# Bind the entry widget to the move focus function
+txt.bind ("<Down>", move_focus)
+# Bind the root window to the show and hide functions
+txt.bind ("<FocusIn>", update_listbox)
+
+# Pack the entry widget
+#txt.pack ()
 
 #
 # Create label 
 #
-lbl = Label(root, text = "Test:, Policy: ", anchor="w", justify="left")
-lbl.grid(column =0, row =1,sticky=W, padx=5)
-lbl.bind('<Double-1>', _clear_button_pressed) 
+lbl = Label(root, text = "", anchor="w", justify="left")
+lbl.grid(column =0, row =1, sticky=W, padx=5)
+#lbl.bind('<Double-1>', _clear_button_pressed) # Useless
 
 #
 # Create button Take
@@ -356,11 +477,26 @@ btn2.bind('<ButtonRelease-1>', _open_button_released)
 #
 # Create button Clear
 #
-btn3 = Button(root, text = "Clear" , width=6,
-             fg = "black", command=_clear_button_pressed)
-btn3.grid(column=2, row=1, padx=5)
+btn3 = Button(root, text = "Clear" , width=6, fg = "black", command=_clear_button_pressed)
+btn3.grid(column=1, row=1, columnspan=2, padx=5, sticky=N)
 
+#
+# Create listbox
+#
+listbox = Listbox (root, width=40, height=5)
+# Pack the listbox widget
+listbox.grid(column=0, row=1, sticky=NW, padx=5, pady=5)
+# Bind the listbox widget to the update function
+listbox.bind ("<Return>", update_entry)
+# Bind the listbox widget to the update function
+listbox.bind ("<Button-1>", move_focus)
+# Bind the listbox widget to the show file name function when item is selected 
+listbox.bind ("<<ListboxSelect>>", show_file_name)
+# Bind the listbox widget to quit when item is selected 
+listbox.bind ("<FocusOut>", lambda e: listbox.grid_remove ())
+            
+# Update the listbox initially
+listbox.grid_remove ()
 
-# Execute Tkinter
-root.mainloop()
-
+# Start the main loop
+root.mainloop ()
